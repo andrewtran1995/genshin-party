652a31b16512784293c8172627aa8a54144ed2fb
Andrew Tran <andrew.tran345@gmail.com>
2024-03-03 23:18:34 -0800

    remove tsup, remove extra commands for debugging test in pipeline

--- src/bin.test.ts
+++ src/bin.test.ts
@@ -11,12 +11,7 @@ beforeAll(async () => {
 	await $`chmod +x dist/bin.js`
 })
 
-// Does not pass in pipeline.
 test('can run bin', async () => {
-	// eslint-disable-next-line unicorn/no-await-expression-member
-	console.log((await $`ls -lrt`).stdout)
-	// eslint-disable-next-line unicorn/no-await-expression-member
-	console.log((await $`ls dist -lrt`).stdout)
 	const command = await $`./dist/bin.js`
 	expect.soft(command.exitCode).toBe(0)
 	expect.soft(command.stdout).toMatch(/Random character: .*/)
